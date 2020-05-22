3a869f734f8f8614aaef4e6292d845ca7227a56d
yangzhongming <yangzhongming@sina.cn>
2020-05-22 12:44:45 +0800

    1

--- stock_eastmoney_db.sql
+++ stock_eastmoney_db.sql
@@ -2,27 +2,6 @@ create database stock_eastmoney_db default char set utf8;
 
 use stock_eastmoney_db;
 
-# hk_mainboard:HK主板
-create table `hk_mainboard` (
-	`dm` varchar(10)  COMMENT '代码',
-	`mc` varchar(30) DEFAULT NULL COMMENT '名称',
-	`zxj` decimal(10,3) DEFAULT NULL COMMENT '最新价',
-	`zde` decimal(10,3)  DEFAULT NULL COMMENT '涨跌额',
-	`zdf` decimal(10,2) DEFAULT NULL COMMENT '涨跌幅',
-	`jk`  decimal(10,3) DEFAULT NULL COMMENT '今开',
-	`zg`  decimal(10,3) DEFAULT NULL COMMENT '最高',
-	`zd`  decimal(10,3) DEFAULT NULL COMMENT '最低',
-	`zs`  decimal(10,3) DEFAULT NULL COMMENT '昨收',
-	`cjl` decimal(20) DEFAULT NULL COMMENT '成交量',
-    `cje` decimal(20) DEFAULT NULL COMMENT '成交额',
-    primary key(dm)
-)  ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-
-select * from hk_mainboard ;
-
-select count(*) from hk_mainboard;
-
-
 create table if not exists hk_stocks (	
 	`dm` varchar(10)  COMMENT '代码',
 	`mc` varchar(30) DEFAULT NULL COMMENT '名称',
@@ -42,23 +21,51 @@ create table if not exists hk_stocks (
     
    
 
-    select * from hk_stocks ;
-    select count(*) from hk_stocks;
-    
-create table `test` (
-	`je1` varchar(10),
-	`je2` decimal(10,2),
-	`je3` int
-);
-insert into test(je1,je2,je3)  values('1',1,1);
-insert into test(je1,je2,je3)  values('0.1',0.1,0.1);
-insert into test(je1,je2,je3)  values('-0.1',-0.1,-0.1);
-insert into test(je1,je2,je3)  values('-',1,1);
-insert into test(je1,je2,je3)  values('-1',-1,-1);
-insert into test(je1,je2,je3)  values('-2',-3,-3);
-insert into test(je1,je2,je3)  values('-3',-1,-3);
-select * from test order by je2 desc;
-####################################################
+select * from hk_stocks ;
+select count(*) from hk_stocks;
+
+
+# hk_mainboard:HK主板
+create table `hk_mainboard` (
+	`dm` varchar(10)  COMMENT '代码',
+	`mc` varchar(30) DEFAULT NULL COMMENT '名称',
+	`zxj` decimal(10,3) DEFAULT NULL COMMENT '最新价',
+	`zde` decimal(10,3)  DEFAULT NULL COMMENT '涨跌额',
+	`zdf` decimal(10,2) DEFAULT NULL COMMENT '涨跌幅',
+	`jk`  decimal(10,3) DEFAULT NULL COMMENT '今开',
+	`zg`  decimal(10,3) DEFAULT NULL COMMENT '最高',
+	`zd`  decimal(10,3) DEFAULT NULL COMMENT '最低',
+	`zs`  decimal(10,3) DEFAULT NULL COMMENT '昨收',
+	`cjl` decimal(20) DEFAULT NULL COMMENT '成交量',
+    `cje` decimal(20) DEFAULT NULL COMMENT '成交额',
+    primary key(dm)
+)  ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+select * from hk_mainboard order by zdf desc limit 20;
+
+select count(*) from hk_mainboard;
+
+# hk_mainboard:创业板
+create table `hk_gem` (
+	`dm` varchar(10)  COMMENT '代码',
+	`mc` varchar(30) DEFAULT NULL COMMENT '名称',
+	`zxj` decimal(10,3) DEFAULT NULL COMMENT '最新价',
+	`zde` decimal(10,3)  DEFAULT NULL COMMENT '涨跌额',
+	`zdf` decimal(10,2) DEFAULT NULL COMMENT '涨跌幅',
+	`jk`  decimal(10,3) DEFAULT NULL COMMENT '今开',
+	`zg`  decimal(10,3) DEFAULT NULL COMMENT '最高',
+	`zd`  decimal(10,3) DEFAULT NULL COMMENT '最低',
+	`zs`  decimal(10,3) DEFAULT NULL COMMENT '昨收',
+	`cjl` decimal(20) DEFAULT NULL COMMENT '成交量',
+    `cje` decimal(20) DEFAULT NULL COMMENT '成交额',
+    primary key(dm)
+)  ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+select * from hk_gem order by zdf desc limit 20;
+
+select count(*) from hk_gem;
+
+
 
 # hk_mainboard:HK知名
 create table `hk_wellknown` (
@@ -76,7 +83,44 @@ create table `hk_wellknown` (
     primary key(dm)
 )  ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
-    select * from hk_wellknown order by zdf desc limit 19 ;
+    select * from hk_wellknown order by zdf desc limit 20 ;
     select count(*) from hk_wellknown;
     
-    select count(*) from hk_stocks t where t.fl = 2
+# hk_bluechips:蓝筹股
+create table `hk_bluechips` (
+	`dm` varchar(10)  COMMENT '代码',
+	`mc` varchar(30) DEFAULT NULL COMMENT '名称',
+	`zxj` decimal(10,3) DEFAULT NULL COMMENT '最新价',
+	`zde` decimal(10,3)  DEFAULT NULL COMMENT '涨跌额',
+	`zdf` decimal(10,2) DEFAULT NULL COMMENT '涨跌幅',
+	`jk`  decimal(10,3) DEFAULT NULL COMMENT '今开',
+	`zg` decimal(10,3) DEFAULT NULL COMMENT '最高',
+	`zd`  decimal(10,3) DEFAULT NULL COMMENT '最低',
+	`zs`  decimal(10,3) DEFAULT NULL COMMENT '昨收',
+	`cjl` decimal(20) DEFAULT NULL COMMENT '成交量',
+    `cje` decimal(20) DEFAULT NULL COMMENT '成交额',
+    primary key(dm)
+)  ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+    select * from hk_bluechips order by zdf desc limit 20 ;
+    select count(*) from hk_bluechips;
+
+# hk_redchips:红筹股 4
+create table `hk_redchips` (
+	`dm` varchar(10)  COMMENT '代码',
+	`mc` varchar(30) DEFAULT NULL COMMENT '名称',
+	`zxj` decimal(10,3) DEFAULT NULL COMMENT '最新价',
+	`zde` decimal(10,3)  DEFAULT NULL COMMENT '涨跌额',
+	`zdf` decimal(10,2) DEFAULT NULL COMMENT '涨跌幅',
+	`jk`  decimal(10,3) DEFAULT NULL COMMENT '今开',
+	`zg` decimal(10,3) DEFAULT NULL COMMENT '最高',
+	`zd`  decimal(10,3) DEFAULT NULL COMMENT '最低',
+	`zs`  decimal(10,3) DEFAULT NULL COMMENT '昨收',
+	`cjl` decimal(20) DEFAULT NULL COMMENT '成交量',
+    `cje` decimal(20) DEFAULT NULL COMMENT '成交额',
+    primary key(dm)
+)  ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+    select * from hk_redchips order by zdf desc limit 20 ;
+    select count(*) from hk_redchips;
+ 
